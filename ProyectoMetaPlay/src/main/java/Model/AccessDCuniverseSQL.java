48508490f0a6bf34d318168b586e638e76b341f9
COMPU-S040\daw1 <pavelmiron6824@gmail.com>
2025-05-07 10:11:19 +0200

    no message

--- ProyectoMetaPlay/src/main/java/Model/AccessDCuniverseSQL.java
+++ ProyectoMetaPlay/src/main/java/Model/AccessDCuniverseSQL.java
@@ -13,7 +13,31 @@ import java.util.List;
 public class AccessDCuniverseSQL {
 
     //acceder a todos videojuegos
+    public List<VideoJuego> getVideojuegos() {
+        List<VideoJuego> productos = new LinkedList<>();
 
+        String prod = "SELECT * FROM Videojuegos";
+
+        try (Connection connection = DataBaseManagerSQL.getConnection(); Statement statement = connection.createStatement();
+             ResultSet dataSet = statement.executeQuery(prod);) {
+            while(dataSet.next()){
+
+                int id = dataSet.getInt(1);
+                String titulo = dataSet.getString(2);
+                java.sql.Date sqlDate = dataSet.getDate(3);
+
+                //convertir DATA de sql en LocalDate java
+                LocalDate fecha_reg = sqlDate.toLocalDate();
+
+                Articulo a =  new Articulo(cod, titulo, fecha_reg);
+                productos.add(a);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return productos;
+    }
 
 
 
