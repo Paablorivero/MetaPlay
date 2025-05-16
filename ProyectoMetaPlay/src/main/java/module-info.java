925f963bf78ed7271107a5e9d1d0fd1e535d92a5
COMPU-S021\daw1 <>
2025-05-16 09:01:13 +0200

    avances controller

--- ProyectoMetaPlay/src/main/java/module-info.java
+++ ProyectoMetaPlay/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.proyectometaplay {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.desktop;
 
 
     opens org.example.proyectometaplay to javafx.fxml;
--- ProyectoMetaPlay/src/main/java/org/example/proyectometaplay/HelloController.java
+++ ProyectoMetaPlay/src/main/java/org/example/proyectometaplay/HelloController.java
@@ -1,16 +1,44 @@
 package org.example.proyectometaplay;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
-public class HelloController {
-    public Button btn_MostrarJuegos;
-    @FXML
-    private Label welcomeText;
+import javafx.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+public class HelloController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+      VBox_MenuPrincipal.setVisible(true);
+        VBox_InicioSesion.setVisible(false);
+        Vbox_Registro.setVisible(false);
     }
-}
+
+    //Paneles de la aplicacion
+    @FXML
+    private VBox VBox_MenuPrincipal;
+
+    @FXML
+    private VBox VBox_InicioSesion;
+
+    @FXML
+    private VBox Vbox_Registro;
+
+    //Botones de la aplicacion
+    @FXML
+    private Button Btn_IniciarSesion;
+
+    @FXML
+    protected void onBtn_IniciarSesion() {
+        VBox_MenuPrincipal.setVisible(false);
+        VBox_InicioSesion.setVisible(true);
+        Vbox_Registro.setVisible(false);
+    }
+
+
+
+}//
